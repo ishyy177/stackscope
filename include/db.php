b439a42f39fdfe8102eac936018a378b15e9f3fa
Ismail <66849050+ishyy177@users.noreply.github.com>
2020-10-28 15:01:25 +0000

    Update db.php

--- include/db.php
+++ include/db.php
@@ -5,7 +5,7 @@
 $servername = "eu-cdbr-west-03.cleardb.net";
 $dbusername = "b28dcd3a62c46f";
 $dbpassword = "3a825ec2";
-$dbname = "newinfo";
+$dbname = "heroku_769aeba104bd939";
 
 $conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
 
