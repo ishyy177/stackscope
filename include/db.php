e6fa7a1535260a155f7b2e3ac0227161f125955e
Ismail <66849050+ishyy177@users.noreply.github.com>
2020-10-28 14:09:02 +0000

    Update db.php

--- include/db.php
+++ include/db.php
@@ -4,7 +4,7 @@
 
 $servername = "eu-cdbr-west-03.cleardb.net";
 $dbusername = "b28dcd3a62c46f";
-$dbpassword = "3a825ec2";
+$dbpassword = "";
 $dbname = "newinfo";
 
 $conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
